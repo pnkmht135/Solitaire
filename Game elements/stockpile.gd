8ffb7a6ce932c3a5d0a8960882deafa8960f3d79
Pankhi Mehta <pnkmht135@gmail.com>
2026-01-14 22:17:59 +0530

    more stockpile functionality

--- Game elements/stockpile.gd
+++ Game elements/stockpile.gd
@@ -7,7 +7,7 @@ extends Table_Element
 #@onready var open_pos: Vector2 =Vector2(100,0) #TODO: make this not hardcoded?
 @onready var click_closed: Area2D = $Click_Closed
 @onready var click_opened: Area2D = $Opened/Click_Opened # TODO: might not need, use card hitbox instead!
-var open_offset = Vector2(50,0)
+var open_offset = Vector2(15,0)
 var step = 3 # TODO: maybe make this non-hardcoded?
 
 func initiate(cards : Array[Card]) -> void:
@@ -24,27 +24,38 @@ func initiate(cards : Array[Card]) -> void:
 
 func _on_click_closed_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("Click"):
-		print("Revealing stock yay")
+		# Do nothing if no cards
+		if cards_stack.is_empty():
+			return
+		# Reset stockpile if at end
+		if current_index == num_cards-1:
+			for card in cards_stack:
+				card.reparent(self)
+				var tween=create_tween()
+				tween.set_ease(Tween.EASE_IN_OUT)
+				tween.tween_property(card,"position",Vector2(0,0),0.5)
+				card.flip_card()
+			current_index=-1
+			return
 		var new_index: int
+		# Typical case: reveal next 3 closed cards
 		if current_index<num_cards-3:# num_card-> last_index+1
 			new_index=current_index+3
+		# Edge case: less than 3 remaining to reveal -> reveal remaining
 		else:
 			new_index=num_cards-1
-		print("Test 1")
 		#TODO: insert func to make all currently open cards fall in line
 		var revealed: Array[Card] = cards_stack.slice(current_index+1,new_index+1)
+		revealed.reverse() # Ensure topmost closed element -> bottommost opened
 		var shift = Vector2(0,0)
-		print("Test 2 ",revealed)
-		print(current_index,new_index)
 		#TODO: Make sure topmost element in array->bottom most in opened
 		for card in revealed:
-			print("bruh")
 			card.reparent(click_opened) #TODO: might be messy beware
 			var tween = create_tween()
 			tween.set_ease(Tween.EASE_IN_OUT)
-			tween.tween_property(card,"position",shift,0.5)
+			tween.tween_property(card,"position",shift,1)
 			shift+=open_offset
-			card.move_to_front()
+			card.move_to_front() # Is this doing anything?
 			card.flip_card()
 			card.disable()
 		current_index=new_index
